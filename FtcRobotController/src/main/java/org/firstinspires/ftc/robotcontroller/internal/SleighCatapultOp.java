82a2e003afcad3d098a3e893b32ef335001c2a69
Alex Arteaga <alexarteaga@Sandras-MacBook-Air.local>
2018-11-20 15:00:40 -0800

    added TensorFlow

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/SleighCatapultOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/SleighCatapultOp.java
@@ -16,21 +16,29 @@ import com.qualcomm.robotcore.util.Range;
 public class SleighCatapultOp extends OpMode {
     //Declare any motors, servos, and sensors
     DcMotor sleighMotor; //40:1 AndyMark Neverest Motor
-    DcMotor catapultMotor; //40:1 AndyMark Neverest Motor
-    Servo tiltServo; //180 Rev Servo
+    DcMotor catapultMotor; //20:1 AndyMark Neverest Motor
+    DcMotor drawerSlideMotor; //40:1 AndyMark Neverest Motor
+    Servo tiltServo1; //180 Rev Servo, left side
+    Servo tiltServo2; //180 Rev Servo, right side
 
     //Declare any variables & constants pertaining to specific robot mechanisms (i.e. drive train)
-    final int COUNTS_PER_REV = 1120;
+    final int COUNTS_PER_REV40 = 1120;
+    final double COUNTS_PER_REV20 = 537.6;
     final double SLEIGH_PWR_MAX = 0.8;
     double currentSleighPwr = 0.0;
-    final double CATAPULT_PWR_MAX = 0.8;
+    final double CATAPULT_PWR_MAX = 0.85;
     double currentCatapultPwr = 0.0;
+    boolean catapultIsOn = false;
+    final double DRAWERSLIDE_PWR_MAX = 0.4;
+    double currentDrawerSlidePwr = 0.0;
     int currentCatapultPos = 0;
     int catapultState = 0;
-    final double TILT_MIN = 0.0;
-    final double TILT_MAX = 1.0;
-    final double TILT_START_POS = 0.5;
+    final double TILT_MIN = 0.0; //Sleigh is Down
+    final double TILT_MAX = 0.45; //Sleigh is Up
+    final double TILT_START_POS = TILT_MIN;
     double currentTiltPos = TILT_START_POS;
+    boolean tiltDown = false;
+    boolean buttonPressed = false;
 
     public SleighCatapultOp() {}
 
@@ -39,10 +47,15 @@ public class SleighCatapultOp extends OpMode {
         sleighMotor = hardwareMap.dcMotor.get("sm");
         sleighMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         catapultMotor = hardwareMap.dcMotor.get("cm");
-        catapultMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        catapultMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        drawerSlideMotor = hardwareMap.dcMotor.get("ds");
+        drawerSlideMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
         //Initialize servos
-        tiltServo = hardwareMap.servo.get("ts");
+        tiltServo1 = hardwareMap.servo.get("ts1");
+        tiltServo1.setDirection(Servo.Direction.FORWARD);
+        tiltServo2 = hardwareMap.servo.get("ts2");
+        tiltServo2.setDirection(Servo.Direction.REVERSE);
 
         telemetry.addData(">", "Press Start to continue");
     }
@@ -62,36 +75,92 @@ public class SleighCatapultOp extends OpMode {
         //Add in update methods for specific robot mechanisms
         updateSleighIntake();
         updateCatapult();
+        updateDrawerSlide();
     }
 
     void updateCatapult() {
+        if (gamepad2.y) { //button to reset catapult encoder
+            DcMotor.RunMode temp = catapultMotor.getMode();
+            catapultMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            catapultMotor.setMode(temp);
+        }
         switch (catapultState) {
             case 0: //wait for launch button to be pressed
                 currentCatapultPwr = 0;
-                if (gamepad2.a) {
+                catapultMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                if (gamepad2.b && !gamepad2.start && currentTiltPos == TILT_MIN) { // b button launches two balls
                     catapultState++;
+                    catapultMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
                     setTime = this.time;
                 }
+                else if (gamepad2.x && currentTiltPos == TILT_MIN) { // x button launches two cubes
+                    catapultState--;
+                    catapultMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    setTime = this.time;
+                }
+                else if (gamepad2.a) { // bring down catapult manually
+                    currentCatapultPwr = -0.30;
+                }
                 break;
+
+            //process to launch BALLS
             case 1: //launch catapult by rotating motor forward
                 currentCatapultPwr = CATAPULT_PWR_MAX;
                 currentCatapultPos = catapultMotor.getCurrentPosition();
-                if (currentCatapultPos >= COUNTS_PER_REV * 0.8) {
+                if (currentCatapultPos >= COUNTS_PER_REV20 * 0.15) {
+                    catapultMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                    catapultMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                     catapultState++;
                     setTime = this.time;
                 }
                 break;
             case 2: //stop catapult motor for 1/2 a second
-                currentCatapultPwr = 0;
                 if (waitSec(0.5)) {
                     catapultState++;
+                    catapultMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                     setTime = this.time;
                 }
                 break;
             case 3: //bring catapult back to original position
-                currentCatapultPwr = -CATAPULT_PWR_MAX;
-                currentCatapultPos = catapultMotor.getCurrentPosition();
-                if (currentCatapultPos <= COUNTS_PER_REV * 0.05) {
+                catapultMotor.setTargetPosition((int)(-COUNTS_PER_REV20 * 0.25));
+                catapultMotor.setPower(CATAPULT_PWR_MAX);
+                //currentCatapultPwr = -CATAPULT_PWR_MAX;
+                //currentCatapultPos = catapultMotor.getCurrentPosition();
+                //if (currentCatapultPos <= COUNTS_PER_REV20 * 0.05) {
+                if (!catapultMotor.isBusy()) {
+                    catapultMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    catapultMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    catapultState = 0;
+                    setTime = this.time;
+                }
+                break;
+
+
+                //process to launch CUBES
+            //process to launch balls
+            case -1: //launch catapult by rotating motor forward
+                catapultMotor.setTargetPosition((int)(COUNTS_PER_REV20 * 0.5));
+                catapultMotor.setPower(CATAPULT_PWR_MAX);
+                if (!catapultMotor.isBusy()) {
+                    catapultMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                    catapultMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    catapultState--;
+                    setTime = this.time;
+                }
+                break;
+            case -2: //stop catapult motor for 1/2 a second
+                if (waitSec(0.5)) {
+                    catapultState--;
+                    catapultMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    setTime = this.time;
+                }
+                break;
+            case -3: //bring catapult back to original position
+                catapultMotor.setTargetPosition((int)(-COUNTS_PER_REV20 * 0.3));
+                catapultMotor.setPower(CATAPULT_PWR_MAX);
+                if (!catapultMotor.isBusy()) {
+                    catapultMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    catapultMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
                     catapultState = 0;
                     setTime = this.time;
                 }
@@ -101,12 +170,26 @@ public class SleighCatapultOp extends OpMode {
 
     void updateSleighIntake() {
         currentSleighPwr = gamepad2.left_stick_y * SLEIGH_PWR_MAX;
-        if (gamepad2.right_bumper) {
+        if (gamepad2.right_bumper && tiltDown) {
             currentTiltPos = TILT_MAX;
+            tiltDown = false;
         }
-        else if (gamepad2.left_bumper) {
+        else if (gamepad2.left_bumper && !tiltDown) {
+            currentTiltPos = TILT_MAX - 0.20;
+            tiltDown = true;
+            setTime = this.time;
+        }
+        if (tiltDown && waitSec(1)) {
+            tiltServo1.getController().pwmEnable();
             currentTiltPos = TILT_MIN;
         }
+        else if (tiltDown && waitSec(0.1)) {
+            tiltServo1.getController().pwmDisable();
+        }
+
+    }
+    void updateDrawerSlide() {
+        currentDrawerSlidePwr = gamepad2.right_stick_y * DRAWERSLIDE_PWR_MAX;
     }
 
     void initialization() {
@@ -114,16 +197,24 @@ public class SleighCatapultOp extends OpMode {
         currentSleighPwr = Range.clip(currentSleighPwr, -SLEIGH_PWR_MAX, SLEIGH_PWR_MAX);
         sleighMotor.setPower(currentSleighPwr);
         currentTiltPos = Range.clip(currentTiltPos, TILT_MIN, TILT_MAX);
-        tiltServo.setPosition(currentTiltPos);
+        tiltServo1.setPosition(currentTiltPos);
+        tiltServo2.setPosition(currentTiltPos);
         //Clip and Initialize Swing Catapult Motor
         currentCatapultPwr = Range.clip(currentCatapultPwr, -CATAPULT_PWR_MAX, CATAPULT_PWR_MAX);
-        catapultMotor.setPower(currentCatapultPwr);
+        if (catapultMotor.getMode() != DcMotor.RunMode.RUN_TO_POSITION) {
+            catapultMotor.setPower(currentCatapultPwr);
+        }
+        currentDrawerSlidePwr = Range.clip(currentDrawerSlidePwr, -DRAWERSLIDE_PWR_MAX, DRAWERSLIDE_PWR_MAX);
+        drawerSlideMotor.setPower(currentDrawerSlidePwr);
     }
     void telemetry() {
         //Show Data for Specific Robot Mechanisms
         telemetry.addData("Sleigh Pwr", sleighMotor.getPower());
         telemetry.addData("Catapult Pwr", catapultMotor.getPower());
-        telemetry.addData("Tilt Pos", tiltServo.getPosition());
+        telemetry.addData("Catapult Enc", catapultMotor.getCurrentPosition());
+        telemetry.addData("Tilt1 Pos", tiltServo1.getPosition());
+        telemetry.addData("Tilt2 Pos", tiltServo2.getPosition());
+        telemetry.addData("DrawerSlide Pwr", drawerSlideMotor.getPower());
     }
 
 
