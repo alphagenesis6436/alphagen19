68375392f999537e8d08e114a4ec936d7b3e215f
Emaneliforp <kietchouam@gmail.com>
2019-07-24 15:41:50 -0700

    TankDrive

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RobotSystems/TankDrive.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RobotSystems/TankDrive.java
@@ -2,6 +2,29 @@ package org.firstinspires.ftc.robotcontroller.internal.RobotSystems;
 
 import org.firstinspires.ftc.robotcontroller.internal.TeleOpMode;
 
-public class TankDrive extends TeleOpMode{
+public class TankDrive extends DriveTrain{
 
+    public void telemetry(){
+        telemetry.addData("TANK DRIVE", "TELEMETRY");
+        telemetry.addData(">>>Left Pwr", motors.get(0).getPower());
+        telemetry.addData(">>>Right Pwr", motors.get(1).getPower());
+    }
+
+    public void update(){
+        for(int i = 0; i < motors.size(); i++)
+            motors.get(i).setPower(clipPower((i%2==0)?-gamepad1.left_stick_y * drivePwrMax:-gamepad1.right_stick_y * drivePwrMax));
+    }
+
+    @Override
+    public void move(double... velocities) {
+
+    }
+
+    public void moveForward(double speed){
+    }
+
+    @Override
+    public void turnClockwise(double angularVelocity) {
+
+    }
 }
--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/TieFighterOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/TieFighterOp.java
@@ -1,18 +1,17 @@
 package org.firstinspires.ftc.robotcontroller.internal;
 
 import org.firstinspires.ftc.robotcontroller.internal.RobotSystems.Mechanism;
+import org.firstinspires.ftc.robotcontroller.internal.RobotSystems.TankDrive;
 
 import java.util.ArrayList;
 
 public class TieFighterOp extends TeleOpMode {
 
     TankDrive tankDrive = new TankDrive();
-    FourBarLift fourBarLift = new FourBarLift();
 
     @Override
     void telemetry() {
         tankDrive.telemetry();
-        fourBarLift.telemtry();
     }
 
     @Override
